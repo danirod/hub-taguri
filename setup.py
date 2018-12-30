bacb1e6d0c97b181fcfb18ecb9b738fd98042467
Dani Rodríguez <dani@danirod.es>
2018-12-30 21:33:55 +0100

    Populate setup.py with valid metadata

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="hub-taguri",
+    version="0.1",
+    description="A Python library for parsing and minting tag URIs in conformance with RFC 4151.",
+    author='Dani Rodríguez',
+    author_email='dani@danirod.es',
+    url='https://danirod.es/pkg/hub-taguri',
+    keywords = "taguri tag uri",
+    packages=['taguri'],
+    license = "ISC",
+)
